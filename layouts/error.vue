b0277fc2c318759866cdf0d296f45fba18e7e170
killer71 <magaky71@gmail.com>
2019-08-23 18:36:15 +0300

    remind

--- layouts/error.vue
+++ layouts/error.vue
@@ -6,7 +6,7 @@
           <span class="d-block display-1 mb-3">404</span>
           Page not found
         </h1>
-        <h1 v-else>An error occurred</h1>
+        <h1 v-else class="text-center overline display-4">An error occurred</h1>
       </v-col>
     </v-row>
 
--- pages/remind/index.vue
+++ pages/remind/index.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="content-center">
-    <form method="post" @submit.prevent="remindPassword">
-      <h1>Remind password</h1>
+  <v-container>
+    <v-row justify="center" style="justify-content: center;">
+      <v-col cols="4" class="text-center">
+        <form method="post" @submit.prevent="remindPassword">
+          <h1 class="display-5 mb-5">Remind password</h1>
 
-      <form-input
-          :type="'email'"
-          :label="'Email'"
-          :item="item"
-          :errors="errors"
-          :property="'username'"
-          @formUpdated="updateValue"
-      ></form-input>
+          <v-text-field
+            :type="'email'"
+            :label="'Email'"
+            :item="item"
+            :errors="errors"
+            :property="'username'"
+            color="#0c5c6f"
+            @formUpdated="updateValue"
+          ></v-text-field>
 
-      <button type="submit" class="btn btn-primary btn-lg">{{ $t('continue') }}</button>
+          <v-btn type="submit" class="mx-auto" color="#0c5c6f" dark>{{ $t('continue') }}</v-btn>
 
-      <p>
-        {{ $t('already_have_account') }}
-        <nuxt-link to="/signin">{{ $t('signin') }}</nuxt-link>
-      </p>
-    </form>
-  </div>
+          <div class="mt-3">
+            {{ $t('already_have_account') }}
+            <v-btn text to="/signin" color="#0c5c6f">{{ $t('signin') }}</v-btn>
+          </div>
+        </form>
+      </v-col>
+    </v-row>
+  </v-container>  
 </template>
 
 <script>
