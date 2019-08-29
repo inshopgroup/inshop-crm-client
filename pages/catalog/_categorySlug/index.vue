8dd4e9c2a3a1a9098dbf0277c6a86e3f60527552
killer71 <magaky71@gmail.com>
2019-08-29 11:25:38 +0300

    Category

--- pages/catalog/_categorySlug/index.vue
+++ pages/catalog/_categorySlug/index.vue
@@ -1,38 +1,43 @@
 <template>
-  <div>
+  <v-container>
     <breadcrumb :breadcrumbs="breadcrumbs"></breadcrumb>
 
-    <section class="jumbotron text-center">
-      <div class="container">
-        <h1 class="jumbotron-heading">E-COMMERCE CATEGORY</h1>
-        <p class="lead text-muted mb-0">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la
-          mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années
-          1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de
-          polices de texte...</p>
-      </div>
-    </section>
+    <v-row>
+      <v-col cols="12">
+        <v-card 
+          color="#ECEFF1" 
+          min-height="230" 
+          class="d-flex flex-column justify-center align-center text-center">
+          <v-card-title>
+            <h1 class="display-1">E-COMMERCE CATEGORY</h1>
+          </v-card-title>
+          <v-card-text>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la
+            mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années
+            1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de
+            polices de texte...</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-sm-3">
-
-        </div>
-        <div class="col">
-          <div class="row">
-            <template v-for="product in products">
-              <product-item :product="product" :category-slug="categorySlug"></product-item>
-            </template>
-          </div>
-        </div>
-      </div>
-    </div>
+    <v-row justify="space-between mt-10">
+      <template v-for="product in products">
+        <v-col cols="auto">
+          <product-item 
+            :product="product" 
+            :category-slug="categorySlug"
+          ></product-item>
+        </v-col>
+      </template>
+    </v-row>
 
     <pagination
-        :total="total"
-        v-model="currentPage"
-        :link-gen="linkGen"
+      :total="total"
+      v-model="currentPage"
+      :link-gen="linkGen"
     ></pagination>
-  </div>
+  </v-container>
 </template>
 
 <script>
