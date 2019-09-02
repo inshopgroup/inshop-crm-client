8a62959a3b9b0c5998ae780d6a2a0cfd0b59db45
killer71 <magaky71@gmail.com>
2019-09-02 18:34:41 +0300

    catalog

--- pages/catalog/_categorySlug/_productSlug/index.vue
+++ pages/catalog/_categorySlug/_productSlug/index.vue
@@ -2,60 +2,56 @@
   <v-container>
     <breadcrumb :breadcrumbs="breadcrumbs"></breadcrumb>
 
-    <v-row>
-      <v-col cols="8">
+    <v-row justify="space-between">
+      <v-col cols="4" offset="1">
 
         <div class="product-info">
 
-          <div class="product-gallery">
-            <div class="product-gallery-thumbnails">
-              <ol class="thumbnails-list list-unstyled">
-                <li><img src="https://via.placeholder.com/350x350/ffcf5b" alt=""></li>
-                <li><img src="https://via.placeholder.com/350x350/f16a22" alt=""></li>
-                <li><img src="https://via.placeholder.com/350x350/d3ffce" alt=""></li>
-                <li><img src="https://via.placeholder.com/350x350/7937fc" alt=""></li>
-                <li><img src="https://via.placeholder.com/350x350/930000" alt=""></li>
-              </ol>
-            </div>
-            <div class="product-gallery-featured">
-              <img src="https://via.placeholder.com/350x350/ffcf5b" alt="">
-            </div>
-          </div>
+          <template v-if="imageSelected">
+            <img :src="imageSelected.contentUrl" class="mb-2" style="width: 100%; cursor: zoom-in;" alt="">
 
-          <div class="mb-5 mt-5">
-            <h2 class="mb-5">Features</h2>
-            <v-row class="mb-5">
-              <v-col cols="3" class="font-weight-bold">Brand:</v-col>
-              <v-col cols="9">Nickony</v-col>
-              <v-col cols="3" class="font-weight-bold">Color:</v-col>
-              <v-col cols="9">Red</v-col>
-              <v-col cols="3" class="font-weight-bold">Size:</v-col>
-              <v-col cols="9">XXL</v-col>
-              <v-col cols="3" class="font-weight-bold">Fabric:</v-col>
-              <v-col cols="9">Cottom</v-col>
+            <v-row>
+              <v-col cols="2" v-for="image in images" :key="image.id">
+                <img @click="selectImage(image.id)" :src="image.contentUrl" alt="" style="width: 100%;">
+              </v-col>
             </v-row>
-            <h2 class="mb-5">Description</h2>
-            <p>{{ translation(product).description }}</p>
-          </div>
+          </template>
+
+          <!-- <v-divider></v-divider> -->
+        
         </div>
       </v-col>
 
-      <v-col cols="4">
-        <h4 class="display-3 mb-4 primary--text">{{ translation(product).name }}</h4>
-        <h2 class="display-2 mb-4">$ 25.00</h2>
-        <!-- <label for="quant">Quantity</label> -->
-        <v-text-field 
-          type="number" 
-          name="quantity" 
-          min="1" 
-          id="quant" 
-          class="mb-5" 
-          label="Choose the quantity"
-        />
-        <v-btn block color="success">Buy Now</v-btn>
+      <v-col cols="6" offset="1">
+        <h4 class="display-3 mb-5">{{ translation(product).name }}</h4>
+        <h2 class="display-2 mb-5">$ 25.00</h2>
+        <v-btn color="primary">
+          <v-icon left>mdi-cart-arrow-down</v-icon>
+          Add to cart
+        </v-btn>
+        
+        <v-row class="mb-5">
+          <v-col cols="12">
+            <h2 class="mb-1">Features</h2>
+          </v-col>
+          <v-col cols="3" class="font-weight-bold">Brand:</v-col>
+          <v-col cols="9">Nickony</v-col>
+          <v-col cols="3" class="font-weight-bold">Color:</v-col>
+          <v-col cols="9">Red</v-col>
+          <v-col cols="3" class="font-weight-bold">Size:</v-col>
+          <v-col cols="9">XXL</v-col>
+          <v-col cols="3" class="font-weight-bold">Fabric:</v-col>
+          <v-col cols="9">Cottom</v-col>
+        </v-row>
       </v-col>
 
+    </v-row>
 
+    <v-row>
+      <v-col cols="12">
+        <h2 class="mb-3">Description</h2>
+        <p>{{ translation(product).description }}</p>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -75,6 +71,40 @@
       await params.store.dispatch('category/getItem', params.params.categorySlug)
       await params.store.dispatch('product/getItem', params.params.productSlug)
     },
+    data() {
+      return {
+        images: [
+          {
+            id: 1,
+            contentUrl: 'https://via.placeholder.com/350x350/ffcf5b',
+          },
+          {
+            id: 2,
+            contentUrl: 'https://via.placeholder.com/350x350/f16a22',
+          },
+          {
+            id: 3,
+            contentUrl: 'https://via.placeholder.com/350x350/d3ffce',
+          },
+          {
+            id: 4,
+            contentUrl: 'https://via.placeholder.com/350x350/7937fc',
+          },
+          {
+            id: 5,
+            contentUrl: 'https://via.placeholder.com/350x350/930000',
+          },
+          {
+            id: 6,
+            contentUrl: 'https://via.placeholder.com/350x350/f16a22',
+          },
+        ],
+        imageSelected: null,
+      }
+    },
+    mounted() {
+      this.imageSelected = this.images[0]
+    },
     computed: {
       breadcrumbs () {
         return [{
@@ -93,6 +123,11 @@
       product () {
         return this.$store.getters['product/item']
       },
+    },
+    methods: {
+      selectImage(id) {
+        [this.imageSelected] = this.images.filter(item => item.id === id)
+      }
     }
   }
 </script>
