8515d37c27c16fc0e5f37954c89d2c9d6893227d
Maksym Mukovoz <info@inshop.com.ua>
2019-09-09 10:09:28 +0200

    Fixed footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,74 +8,74 @@
     <v-container>
       <v-row no-gutters justify="space-between">
         <v-col cols="12" lg="4" md="5" sm="12">
-          <h5>About</h5>
+          <h2>About</h2>
           <v-divider class="mt-0 mb-2" color="white" width="25%" />
           <p class="mb-3">Le Lorem Ipsum est simplement du faux texte employé dans la
           composition et la mise en page avant impression.</p>
         </v-col>
 
         <v-col cols="12" lg="2" md="2" sm="4">
-          <h5>Informations</h5>
+          <h2>Informations</h2>
           <v-divider class="mt-0 mb-2" color="white" width="25%" />
           <ul class="list-unstyled pl-0">
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/about">About</a>
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/about">About</a>
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/about">About</a>
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/about">About</a>
             </li>
           </ul>
         </v-col>
 
         <v-col cols="12" lg="3" md="2" sm="4">
-          <h5>Others links</h5>
+          <h2>Others links</h2>
           <v-divider class="mt-0 mb-2" color="white" width="25%" />
           <ul class="list-unstyled pl-0">
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/terms-of-use">Terms of use</a>
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/terms-of-use">Terms of use</a>
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/terms-of-use">Terms of use</a>
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-link</v-icon>
+              <v-icon left>mdi-link</v-icon>
               <a class="white--text" href="/page/terms-of-use">Terms of use</a>
             </li>
           </ul>
         </v-col>
         <v-col cols="12" lg="3" md="3" sm="4">
-          <h5>Contact</h5>
+          <h2>Contact</h2>
           <v-divider class="mt-0 mb-2" color="white" width="25%" />
-          <ul class="list-unstyled pl-0">
+          <ul class="pl-0">
             <li class="mb-2">
-              <v-icon small left>mdi-home</v-icon>My company
+              <v-icon left>mdi-home</v-icon>My company
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-email-edit</v-icon>
+              <v-icon left>mdi-email-edit</v-icon>
               <a href="mailto:magaky71@gmail.com" class="white--text">email@example.com</a>
             </li>
             <li class="mb-2">
-              <v-icon small left>mdi-phone</v-icon>
+              <v-icon left>mdi-phone</v-icon>
               + 33 12 14 15 16
             </li>
             <li>
-              <v-icon small left>mdi-phone</v-icon>
+              <v-icon left>mdi-phone</v-icon>
               + 33 12 14 15 16
             </li>
           </ul>
@@ -85,15 +85,10 @@
         <v-col cols="auto">
           <p class="mb-0">
             <span>Inshop ecommerce, v0.2.0, Powered by</span>
-            <a class="success--text" href="https://inshopgroup.com/" target="_blank">Inshop Group</a>
+            <a class="white--text" href="https://inshopgroup.com/" target="_blank">Inshop Group</a>
           </p>
         </v-col>
       </v-row>
     </v-container>
   </v-footer>
 </template>
-
-<script>
-  export default {}
-</script>
-
