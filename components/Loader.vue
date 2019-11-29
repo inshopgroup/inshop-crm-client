6299e079a9530c17c7beca51315c834fb784596f
Maksym Mukovoz <info@inshop.com.ua>
2019-11-29 14:23:47 +0100

    Updated loader

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,41 +1,21 @@
 <template>
-  <div class="ajax-loader" v-if="isLoading"></div>
+  <v-overlay :value="isLoading">
+    <v-progress-circular
+        v-if="isLoading"
+        :size="50"
+        color="primary"
+        indeterminate
+    ></v-progress-circular>
+  </v-overlay>
 </template>
 
 <script>
-export default {
-  name: 'Loader',
-  data () {
-    return {
-    }
-  },
-  computed: {
-    isLoading () {
-      return this.$store.state.isLoading
+  export default {
+    name: 'Loader',
+    computed: {
+      isLoading() {
+        return this.$store.state.isLoading
+      }
     }
   }
-}
 </script>
-
-<style>
-  .ajax-loader {
-    opacity: 0.5;
-    position: fixed;
-    border-radius: 5px;
-    width: 100%;
-    height: 100%;
-    background: #fff url("../assets/loader-eclipse.gif") no-repeat center;
-    left:0;
-    top:0;
-    z-index: 999999;
-  }
-  .ajax-loader:after {
-    content: '';
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: block;
-  }
-</style>
